f1a0dc9fa80fe912b98e55b749f240f7c9025caa
Tim Maddux <timmaddux@Tims-MacBook-Pro.local>
2016-03-17 15:02:00 -0500

    While technically I met all the requirements, I'd like to go back and clean up CSS and make the delete buttons on the additional music work correctly.

--- songiife.js
+++ songiife.js
@@ -1,20 +1,40 @@
 "use strict"
-function buildDomNew (arg) {
+function buildDomNew (arg1) {
     let del = document.getElementsByClassName("deleteButton");
-    let addYes = document.getElementById("rightColumn");
+    let addYes = document.getElementById("newStuff");
     let string = "";
-    for (let i = 0; i < arg.length; i++) {
-    string += `<section><h3 class ="song">${arg[i].song}</h3><h5 class="artist">${arg[i].artist}</h5> | <h5 class="album">${arg[i].album}</h5><input type='button' class='deleteButton' value='X'></div></section>`;
+    for (let i = 0; i < arg1.length; i++) {
+        string += `<section class="newSong"><input type='button' class='deleteButton' value='X'></div><h3 class ="song">${arg1[i].song}</h3><h5 class="artist">${arg1[i].artist}</h5> | <h5 class="album">${arg1[i].album}</h5></section>`;
     }
     addYes.innerHTML = string;
-    for (let i = 0; i <arg.length; i++) {
+    for (let i = 0; i <arg1.length; i++) {
         del[i].addEventListener('click', deleteCard);
     }
+    function deleteCard () {
+        this.parentNode.parentNode.removeChild(this.parentNode);
+    } 
+    document.getElementById("moreButton").innerHTML = "<button id='moreButton' class='buttonText'>More ></button>";
 };
 
-function deleteCard () {
-    this.parentNode.parentNode.removeChild(this.parentNode);
-    console.log(this.parentNode.parentNode);
+function buildDomClassic (arg2) {    
+    // let deleteButton = document.getElementsByClassName("deleteButton");
+    let classics = document.getElementById("classics");
+    let string = "";
+    document.getElementById("moreButton").addEventListener("click", loadMore);
+    // function deleteCard () {
+    //     console.log(1234 + 1234, "bacon");
+    // }
+    function loadMore () {
+        for (let i = 0; i < arg2.length; i++) {
+            string += `<section class="newSong"><input type='button' class='deleteButton' value='X'></div><h3 class ="song">${arg2[i].song}</h3><h5 class="artist">${arg2[i].artist}</h5> | <h5 class="album">${arg2[i].album}</h5></section>`;
+        }
+        // for (let i = 0; i <arg2.length; i++) {
+        //     deleteButton[i].addEventListener('click', deleteCard);
+        //     deleteButton[i].classList.add("hi");
+        // }
+    classics.innerHTML = string;
+    }
 };
 
 SongLoader.loadNewStuff(buildDomNew);
+SongLoader.loadClassics(buildDomClassic);
--- songloader.js
+++ songloader.js
@@ -12,21 +12,30 @@ let SongLoader = (function () {
                 newHits = JSON.parse(this.responseText).newStuffObj;
                 newCallback(newHits); 
             });
+        },
+        loadClassics: function (classicCallback) {
+            let loader = new XMLHttpRequest();
+            loader.open("GET", "classics.json"); 
+            loader.send();
+            loader.addEventListener("load", function () {
+                classicHits = JSON.parse(this.responseText).classicsObj;
+                classicCallback(classicHits); 
+            })
         }
     }
 })();
 
-document.getElementById("addMusicView").classList.add("hidden");
+// document.getElementById("addMusicView").classList.add("hidden");
 
-document.getElementById("addMusicLink").addEventListener("click", function () {
-    document.getElementById("listMusicView").classList.add("hidden");
-    document.getElementById("addMusicView").classList.remove("hidden");
-})
+// document.getElementById("addMusicLink").addEventListener("click", function () {
+//     document.getElementById("listMusicView").classList.add("hidden");
+//     document.getElementById("addMusicView").classList.remove("hidden");
+// })
 
-document.getElementById("listMusicLink").addEventListener("click", function () {
-    document.getElementById("listMusicView").classList.remove("hidden");
-    document.getElementById("addMusicView").classList.add("hidden");
-})
+// document.getElementById("listMusicLink").addEventListener("click", function () {
+//     document.getElementById("listMusicView").classList.remove("hidden");
+//     document.getElementById("addMusicView").classList.add("hidden");
+// })
 
 
 
